1c8f62a719a916eea4be0a00ec07b1bd8af75271
imomo <imoimo.roize@gmail.com>
2022-01-16 00:32:16 +0900

    ♻️ 集計機能をcronでできるようにした

--- total.py
+++ total.py
@@ -1,60 +1,66 @@
-import schedule, time, json, datetime
-import discord
+import datetime,calendar
+import discord,json
 
 client = discord.Client()
 notice_channel = 917413934162649088
 
-point_list = []
-is_done = False
+def get_end_month(dt):
+    return calendar.monthrange(dt.year,dt.month)
+
+def get_aggregation_period(dt):
+    if dt.day == 1:
+        last_date = (dt.replace(day=1) - datetime.timedelta(days=1)).replace(day=1)
+        end_month = get_end_month(last_date)[1]
+        date_first = last_date.replace(day=int(end_month/2)+1)
+        date_end = last_date.replace(day=end_month)
+    else:
+        date_first = dt.replace(day=1)
+        date_end = dt.replace(day=int(get_end_month(dt)[1]/2))
+    return date_first.strftime('%Y/%m/%d'),date_end.strftime('%Y/%m/%d')
+
 def total():
-	global point_list, is_done
-	json_file = open("./userinfo.json")
-	json_value = json.load(json_file)
-	json_file.close()
+    json_file = open("./userinfo.json")
+    json_value = json.load(json_file)
+    json_file.close()
 
-	point_list = []
-	for v in json_value:
-		point_list.append( [json_value[v]["point"], v] )
-		json_value[v]["point"] = 0
+    point_list = []
+    for v in json_value:
+        point_list.append( [json_value[v]["point"], v] )
+        json_value[v]["point"] = 0
 
-	json_write = open("./userinfo.json", "w")
-	json.dump(json_value, json_write)
-	json_write.close()
+    json_write = open("./userinfo.json", "w")
+    json.dump(json_value, json_write)
+    json_write.close()
 
-	point_list.sort(reverse=True)
-	is_done = True
-
-
-schedule.every(5).seconds.do(total)
+    point_list.sort(reverse=True)
+    return point_list
 
 @client.event
 async def on_ready():
-	global point_list, is_done
-	while True:
-		schedule.run_pending()
+    date_now = datetime.datetime.now()
+    date_first,date_end = get_aggregation_period(date_now)
+    description = f"期間: {date_first} ~ {date_end} \n"
 
-		if(is_done):
-			date_now = datetime.datetime.now()
-			date_2w_ago = date_now - datetime.timedelta(weeks = 2)
-			description = f"期間: {date_2w_ago} ~ {date_now} \n"
+    rank = 1
+    description += "ポイントがリセットされました。 \n"
+    for v in total():
+        member = await client.guilds[0].fetch_member(int(v[1]))
+        name = member.display_name
+        description += f"{rank}位: {name} さん ({v[0]}ポイント) \n"
+        rank += 1
+    
+    embed = discord.Embed(
+        title = "ポイント集計",
+        description = description
+    )
+    await client.get_channel(notice_channel).send(embed = embed)
+    await client.close()
 
-			rank = 1
-			description += "ポイントがリセットされました。 \n"
-			for v in point_list:
-				member = await client.guilds[0].fetch_member(int(v[1]))
-				name = member.display_name
-				description += f"{rank}位: {name} さん ({v[0]}ポイント) \n"
-				rank += 1
-			
-			embed = discord.Embed(
-				title = "ポイント集計",
-				description = description
-			)
-
-			await client.get_channel(notice_channel).send(embed = embed)
-
-		is_done = False
-		time.sleep(2)
+dt = datetime.datetime.now()
+if not int(dt.day) in [1,int(get_end_month(dt)[1]/2)+1]:
+    exit()
+date_first,date_end = get_aggregation_period(dt)
+print(date_first,date_end)
 
 TOKEN_file = open(".TOKEN", "r", encoding="utf-8")
 TOKEN = TOKEN_file.read()
